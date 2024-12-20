4b3cb5f0ee9d54fe4df82c3dc095018def6d06ca
LionTheGreatRealFrFr <topgtatey@gmail.com>
2024-12-20 15:12:30 +0800

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -1,5 +1,5 @@
 -- Collection of strings later formatted by the decompiler for output
-local MemeStrings = {" boo 👻", " Exceeded decompiler timeout.", " DECOMPILED BY ADVANCED DECOMPILER V3", " Decompiled with the Synapse X Luau decompiler.", " 𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻", "SynapseX Decompiler", "decompiler is slow, removed right now :(", "NOTE: Currently in beta! Not representative of final product.", " params : ...", " " .. os.date(), " your advertisement could be here"}
+local MemeStrings = {" Decompiled With the Skibidi Decompiler11!!1", " Synapse did this1!!"}
 
 local Strings = {
 	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
