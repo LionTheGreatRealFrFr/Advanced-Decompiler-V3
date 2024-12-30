683cb1a0de171e96a2060161742729d6e0cfe2b2
LionTheGreatRealFrFr <topgtatey@gmail.com>
2024-12-30 11:54:31 +0800

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -1,13 +1,13 @@
 -- Collection of strings later formatted by the decompiler for output
-local MemeStrings = {" Decompiled With the Skibidi Decompiler11!!1", " Synapse did this1!!"}
+local MemeStrings = {" Decompiled With SAD! Decompiler!", " Script Was 1 Tapped", " Skibidi Dom Dom Dom Yes Yes"}
 
 local Strings = {
-	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
-	TIMEOUT = "-- DECOMPILER TIMEOUT",
-	COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
-	UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
-	USED_GLOBALS = "-- USED GLOBALS: %s.\n",
-	DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
+	SUCCESS = "-- [SAD!]" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
+	TIMEOUT = "-- [SAD!] DECOMPILER TIMEOUT",
+	COMPILATION_FAILURE = "-- [SAD!] SCRIPT FAILED TO COMPILE, ERROR:\n%s",
+	UNSUPPORTED_LBC_VERSION = "-- [SAD!] PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
+	USED_GLOBALS = "-- [SAD!] USED GLOBALS: %s.\n",
+	DECOMPILER_REMARK = "-- [SAD!] DECOMPILER REMARK: %s\n"
 }
 
 return Strings
